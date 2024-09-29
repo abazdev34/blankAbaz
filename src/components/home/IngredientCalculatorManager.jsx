205d894df1344df01132126f2ab5b28b2efa06d7
abazdev34 <abaztoktorbaev89@gmail.com>
2024-09-29 07:54:30 +0300

    fsdfdsfsfsd

--- src/components/home/IngredientCalculatorManager.jsx
+++ src/components/home/IngredientCalculatorManager.jsx
@@ -7,7 +7,7 @@ const IngredientCalculator = () => {
     const [totalWeight, setTotalWeight] = useState(0);
     const [multiplier, setMultiplier] = useState(1); // Keep multiplier for calculations
 
-    const quickSelectDishes = ['пико', 'курицаМаринад', 'соустако', 'рисзапровка'];
+    const quickSelectDishes = ['пико', 'курицаМаринад', 'соустако', 'рисзапровка','специКурица'];
 
     const handleDishSelect = (dish) => {
         setSelectedDish(dish);
