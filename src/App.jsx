aa24f4cfdd1da8faff6c5e52b6dae6cdc2a5711c
abazdev34 <abaztoktorbaev89@gmail.com>
2024-11-18 07:57:37 +0300

    fdfdfdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+
 import { Route,  Routes } from 'react-router-dom';
 
 import './App.scss';
@@ -9,27 +9,10 @@ import Timer_ovoshi from './components/home/ovoshi'
 
 
 const App = () => {
-  const [inventory, setInventory] = useState({});
-  const [receiveHistory, setReceiveHistory] = useState([]);
-  const [sendHistory, setSendHistory] = useState([]);
 
   // Локалдык сактоодон инвентаризацияны жүктөө
-  useEffect(() => {
-    const savedInventory = localStorage.getItem('inventory');
-    const savedReceiveHistory = localStorage.getItem('receiveHistory');
-    const savedSendHistory = localStorage.getItem('sendHistory');
-    if (savedInventory) setInventory(JSON.parse(savedInventory));
-    if (savedReceiveHistory) setReceiveHistory(JSON.parse(savedReceiveHistory));
-    if (savedSendHistory) setSendHistory(JSON.parse(savedSendHistory));
-  }, []);
 
-  // Инвентаризацияны жана тарыхты локалдык сактоого жазуу
-  useEffect(() => {
-    localStorage.setItem('inventory', JSON.stringify(inventory));
-    localStorage.setItem('receiveHistory', JSON.stringify(receiveHistory));
-    localStorage.setItem('sendHistory', JSON.stringify(sendHistory));
-  }, [inventory, receiveHistory, sendHistory]);
-
+ 
   // Товарды кабыл алуу функциясы
 
   // Товарды жөнөтүү функциясы
